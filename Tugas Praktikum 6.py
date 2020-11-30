eeaaaea562cd5f2007a28af90f616ea024cf752d
mirashintania <mirashintania@gmail.com>
2020-11-30 21:29:11 +0700

    Tugas Pertemuan 10

---
+++ Tugas Praktikum 6.py
@@ -0,0 +1,77 @@
+dataMhs = {}
+print("==================================================================")
+print("|              PROGRAM INPUT NILAI MAHASISWA                     |")
+print("==================================================================")
+
+while True:
+    c = input("\nA)dd, E)dit, D)elete L)ist, Q)uit: ")
+
+    ## MENU TAMBAH DATA
+    if (c.lower() == 'a'):
+        print("\n=========================")
+        print("| TAMBAH DATA MAHASISWA |")
+        print("=========================")
+        nama = input("Masukkan Nama        : ")
+        nim = input("Masukkan NIM         : ")
+        nilaiTugas = int(input("Masukkan Nilai Tugas : "))
+        nilaiUts = int(input("Masukkan Nilai UTS   : "))
+        nilaiUas = int(input("Masukkan Nilai UAS   : "))
+        nilaiAkhir = (0.30 * nilaiTugas) + (0.35 * nilaiUts) + (0.35 * nilaiUas)
+        dataMhs[nama] = nim, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir
+        print("\nData Berhasil Ditambahkan!")
+
+    ## MENU TAMPILKAN DATA
+    elif (c.lower() == 'l'):
+        if dataMhs.items():
+            print("\n                      DAFTAR NILAI MAHASISWA                    ")
+            print("==================================================================")
+            print("| No |     Nama     |    NIM    | Tugas |  UTS  |  UAS  |  Akhir |")
+            print("==================================================================")
+            i = 0
+            for x in dataMhs.items():
+                i += 1
+                print("| {6:2} | {0:12s} | {1:9s} | {2:5} | {3:5} | {4:5} | {5:6} |".format(x[0], x[1][0], x[1][1],
+                                                                                            x[1][2], x[1][3], x[1][4],
+                                                                                            i))
+            print("==================================================================")
+        else:
+            print("\n                      DAFTAR NILAI MAHASISWA                    ")
+            print("==================================================================")
+            print("| No |     Nama     |    NIM    | Tugas |  UTS  |  UAS  |  Akhir |")
+            print("==================================================================")
+            print("|                          TIDAK ADA DATA!                       |")
+            print("==================================================================")
+
+    ## MENU HAPUS DATA
+    elif (c.lower() == 'd'):
+        nama = input("Masukkan Nama:  ")
+        if nama in dataMhs.keys():
+            del dataMhs[nama]
+            print("Data Telah dihapus!")
+        else:
+            print("Data Mahasiswa Tidak Ada".format(nama))
+
+    ## MENU UBAH DATA
+    elif (c.lower() == 'e'):
+        print("\n=======================")
+        print("| EDIT DATA MAHASISWA |")
+        print("=======================")
+        nama = input("Masukkan Nama: ")
+        if nama in dataMhs.keys():
+            nim = input("Masukkan NIM Baru         : ")
+            nilaiTugas = int(input("Masukkan Nilai Tugas Baru : "))
+            nilaiUts = int(input("Masukkan Nilai UTS Baru   : "))
+            nilaiUas = int(input("Masukkan Nilai UAS Baru   : "))
+            nilaiAkhir = (0.30 * nilaiTugas) + (0.35 * nilaiUts) + (0.35 * nilaiUas)
+            dataMhs[nama] = nim, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir
+            print("\nData Berhasil Di Update!")
+        else:
+            print("Data tidak ditemukan!")
+
+    ## MENU KELUAR PROGRAM
+    elif (c.lower() == 'q'):
+        print("\n Mira SHintania \n 312010290 \n TI.20.B2")
+        break
+
+    else:
+        print("Pilih menu yang tersedia: ")
